afe94dba80cfc26da084ba38fa6b12d794f6f45a
MARIO <mariosergiovaldes145@gmail.com>
2020-06-20 16:53:29 -0500

    upgrade pip

--- Lib/site-packages/pip/_internal/req/__init__.py
+++ Lib/site-packages/pip/_internal/req/__init__.py
@@ -1,12 +1,19 @@
+# The following comment should be removed at some point in the future.
+# mypy: strict-optional=False
+
 from __future__ import absolute_import
 
 import logging
 
+from pip._internal.utils.logging import indent_log
+from pip._internal.utils.typing import MYPY_CHECK_RUNNING
+
+from .req_file import parse_requirements
 from .req_install import InstallRequirement
 from .req_set import RequirementSet
-from .req_file import parse_requirements
-from pip._internal.utils.logging import indent_log
 
+if MYPY_CHECK_RUNNING:
+    from typing import Any, List, Sequence
 
 __all__ = [
     "RequirementSet", "InstallRequirement",
@@ -16,8 +23,24 @@ __all__ = [
 logger = logging.getLogger(__name__)
 
 
-def install_given_reqs(to_install, install_options, global_options=(),
-                       *args, **kwargs):
+class InstallationResult(object):
+    def __init__(self, name):
+        # type: (str) -> None
+        self.name = name
+
+    def __repr__(self):
+        # type: () -> str
+        return "InstallationResult(name={!r})".format(self.name)
+
+
+def install_given_reqs(
+    to_install,  # type: List[InstallRequirement]
+    install_options,  # type: List[str]
+    global_options=(),  # type: Sequence[str]
+    *args,  # type: Any
+    **kwargs  # type: Any
+):
+    # type: (...) -> List[InstallationResult]
     """
     Install everything in the given list.
 
@@ -30,13 +53,12 @@ def install_given_reqs(to_install, install_options, global_options=(),
             ', '.join([req.name for req in to_install]),
         )
 
+    installed = []
+
     with indent_log():
         for requirement in to_install:
-            if requirement.conflicts_with:
-                logger.info(
-                    'Found existing installation: %s',
-                    requirement.conflicts_with,
-                )
+            if requirement.should_reinstall:
+                logger.info('Attempting uninstall: %s', requirement.name)
                 with indent_log():
                     uninstalled_pathset = requirement.uninstall(
                         auto_confirm=True
@@ -50,7 +72,7 @@ def install_given_reqs(to_install, install_options, global_options=(),
                 )
             except Exception:
                 should_rollback = (
-                    requirement.conflicts_with and
+                    requirement.should_reinstall and
                     not requirement.install_succeeded
                 )
                 # if install did not succeed, rollback previous uninstall
@@ -59,11 +81,12 @@ def install_given_reqs(to_install, install_options, global_options=(),
                 raise
             else:
                 should_commit = (
-                    requirement.conflicts_with and
+                    requirement.should_reinstall and
                     requirement.install_succeeded
                 )
                 if should_commit:
                     uninstalled_pathset.commit()
-            requirement.remove_temporary_source()
 
-    return to_install
+            installed.append(InstallationResult(requirement.name))
+
+    return installed
